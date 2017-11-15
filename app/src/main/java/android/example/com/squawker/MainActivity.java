5f240cb67e62eeacd3c20c2f5519d811a952f872
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-14 23:02:22 -0500

    Finish TFCM.02

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -86,9 +86,12 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
-        // TODO (1) Get the test data here from the extras bundle that came with this intent.
+        // COMPLETED (1) Get the test data here from the extras bundle that came with this intent.
         // To confirm that the data was passed in, make sure to show the data in a log statement.
-
+        if (getIntent().getExtras() != null) {
+            String name = getIntent().getExtras().getString("name");
+            Log.i(LOG_TAG, name);
+        }
     }
 
     @Override
